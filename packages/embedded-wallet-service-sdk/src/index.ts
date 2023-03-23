4b14e067e269aff3a5c947bbd054ca844709c322
ElasticBottle <winstonyeo99@yahoo.com>
2023-03-23 14:50:48 -0700

    chore: update formatting

--- packages/embedded-wallet-service-sdk/src/index.ts
+++ packages/embedded-wallet-service-sdk/src/index.ts
@@ -1,6 +1,6 @@
+export * from "./constants/settings";
 export * from "./interfaces/Auth";
 export * from "./interfaces/EmbeddedWallets/EmbeddedWallets";
 export * from "./interfaces/EmbeddedWallets/GaslessTransactionMaker";
 export * from "./interfaces/EmbeddedWallets/Signer";
 export * from "./lib/Paper";
-export * from "./constants/settings";
