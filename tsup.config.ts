49fa3c6ff2bf0d7e2bd8f651b6e2cbb905be37c1
ElasticBottle <winstonyeo99@yahoo.com>
2023-04-14 23:25:35 -0400

    chore: output esm only build for ews connectors

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,8 +4,9 @@ export default defineConfig((options) => {
   return {
     sourcemap: true,
     dts: true,
+    splitting: true,
     clean: !options.watch,
-    format: ["cjs", "esm"],
+    format: options.env?.ESM_ONLY ? ["esm"] : ["cjs", "esm"],
     minify: !options.watch,
     // Internal packages not meant for client consumption should be here
     noExternal: [],
