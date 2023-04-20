1ef9180e9835d6c29b406fd533a4891ffebb2a2f
ElasticBottle <winstonyeo99@yahoo.com>
2023-04-20 16:31:38 -0400

    feat(arbitrary evm rpc): add support for arbitrary evm support via rpcEndpoint override

--- packages/sdk-common-utilities/src/constants/blockchain.ts
+++ packages/sdk-common-utilities/src/constants/blockchain.ts
@@ -1,6 +1,6 @@
 export const ChainToPublicRpc: Record<Chain, string> = {
   Ethereum: "https://rpc.ankr.com/eth",
-  Goerli: "https://eth-goerli.g.alchemy.com/v2/demo",
+  Goerli: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
   Mumbai: "https://rpc-mumbai.maticvigil.com",
   Polygon: "https://rpc-mainnet.maticvigil.com",
   Avalanche: "https://api.avax.network/ext/bc/C/rpc",
