633ab12a79fd33d76522a6ba152e5a160f687402
ElasticBottle <winstonyeo99@yahoo.com>
2023-04-27 12:56:40 -0400

    chore: remove old and unused components

--- packages/react-client-sdk/src/index.ts
+++ packages/react-client-sdk/src/index.ts
@@ -5,15 +5,8 @@ export {
 export type { PaperSDKError, PaperUser } from "@paperxyz/js-client-sdk";
 // re-export types and enums
 export type { Locale } from "@paperxyz/sdk-common-utilities";
-export * from "./Provider";
 export * from "./components/CheckoutWithCard";
-export * from "./components/CreateWallet";
-export * from "./components/LoginWithPaper";
 export * from "./components/PaperCheckout";
-export * from "./components/PayWithCard";
-export * from "./components/PayWithCrypto/index";
-export * from "./components/VerifyOwnershipWithPaper";
-export * from "./components/checkoutWithEth/index";
 export * from "./interfaces/CustomContract";
 export * from "./interfaces/PaymentSuccessResult";
 export * from "./interfaces/TransferSuccessResult";
