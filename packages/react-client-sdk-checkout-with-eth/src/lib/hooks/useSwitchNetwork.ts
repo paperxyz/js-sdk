89a658053a064459376ba7d6924668b005abf02f
gerrysaporito <gerrysaporito@gmail.com>
2023-07-05 11:57:18 -0700

    .

--- packages/react-client-sdk-checkout-with-eth/src/lib/hooks/useSwitchNetwork.ts
+++ packages/react-client-sdk-checkout-with-eth/src/lib/hooks/useSwitchNetwork.ts
@@ -2,7 +2,7 @@ import type { ethers } from "ethers";
 import { useCallback } from "react";
 
 import type { Chain } from "wagmi";
-import { useConnect, useSwitchNetwork as useSwitchNetworkWagmi } from "wagmi";
+import { useSwitchNetwork as useSwitchNetworkWagmi } from "wagmi";
 import { WagmiChains } from "../../components/checkoutWithEth";
 
 export const useSwitchNetwork = ({
@@ -13,7 +13,6 @@ export const useSwitchNetwork = ({
   switchNetworkAsync: (chainId: number) => Promise<Chain>;
 } => {
   const { switchNetworkAsync: _switchNetworkAsync } = useSwitchNetworkWagmi();
-  const { connectors } = useConnect();
 
   const switchNetworkAsync = useCallback(
     async (chainId: number) => {
@@ -26,7 +25,9 @@ export const useSwitchNetwork = ({
           throw new Error(error);
         }
         if ((await signer.getChainId()) !== chainId) {
-          const error = `Error switching chain. Please switch your network to ${chainToSwitchTo?.name}`;
+          const error = `Error switching chain. Please switch your network to ${
+            chainToSwitchTo?.name ?? `chain with chainId: ${chainId}`
+          }`;
           throw new Error(error);
         }
         return chainToSwitchTo;
