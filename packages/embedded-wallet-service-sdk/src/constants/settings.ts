e4d003fefeed6b786747e92f44a9e75b5b1bf07a
Winston Yeo <44563205+ElasticBottle@users.noreply.github.com>
2023-07-05 04:43:28 -0400

    fix(embedded-wallet-service-sdk):  add new local storage constant for user wallet details (#115)
    * chore(embedded-wallet-service): release new internal name for user wallet detail
    
    * chore(embedded-wallet-service-sdk): add changeset

--- packages/embedded-wallet-service-sdk/src/constants/settings.ts
+++ packages/embedded-wallet-service-sdk/src/constants/settings.ts
@@ -1,5 +1,7 @@
 export const EMBEDDED_WALLET_PATH = "/sdk/2022-08-12/embedded-wallet";
 
+export const WALLET_USER_DETAILS_LOCAL_STORAGE_NAME = (clientId: string) =>
+  `paperEwsWalletUserDetails-${clientId}`;
 export const WALLET_USER_ID_LOCAL_STORAGE_NAME = (clientId: string) =>
   `paperEwsWalletUserId-${clientId}`;
 const AUTH_TOKEN_LOCAL_STORAGE_PREFIX = "walletToken";
