f8ee7ceba81f76bd3722b36bbdcbc1fbfa260b99
gerrysaporito <gerrysaporito@gmail.com>
2023-07-05 11:50:02 -0700

    .

--- packages/react-client-sdk-checkout-with-eth/src/lib/utils/handleError.ts
+++ packages/react-client-sdk-checkout-with-eth/src/lib/utils/handleError.ts
@@ -12,6 +12,7 @@ export function handlePayWithCryptoError(
   onError?: (code: PaperSDKError) => void,
   postToParent?: (errorObject: Omit<IErrorObject, "isErrorObject">) => void,
 ) {
+  // No error passed.
   if (!error) {
     const error = new Error("Something went wrong, contact support.");
     onError?.({ code: PayWithCryptoErrorCode.ErrorSendingTransaction, error });
@@ -19,50 +20,53 @@ export function handlePayWithCryptoError(
       title: PayWithCryptoErrorCode.ErrorSendingTransaction,
       description: "Something went wrong, contact support.",
     });
+    // Custom error object.
   } else if ("isErrorObject" in error) {
     onError?.({ code: error.title, error: new Error(error.title) });
     postToParent?.({ ...error });
+    // Users denied the transaction.
+  } else if (
+    error?.message?.includes("rejected") ||
+    error?.message?.includes("denied transaction")
+  ) {
+    onError?.({ code: PayWithCryptoErrorCode.TransactionCancelled, error });
+    postToParent?.({
+      title: PayWithCryptoErrorCode.TransactionCancelled,
+      description: "",
+    });
+    // Insufficient funds.
+  } else if (error?.message?.includes("insufficient funds")) {
+    onError?.({
+      code: PayWithCryptoErrorCode.InsufficientBalance,
+      error,
+    });
+    postToParent?.({
+      title: PayWithCryptoErrorCode.InsufficientBalance,
+      description: `Check your wallet's ETH balance to make sure you have enough`,
+    });
+    // Failed to switch chains
+  } else if (error?.message?.includes("Error switching chain")) {
+    onError?.({
+      code: PayWithCryptoErrorCode.ChainSwitchUnderway,
+      error,
+    });
+    postToParent?.({
+      title: PayWithCryptoErrorCode.ChainSwitchUnderway,
+      description: "Check your wallet app",
+    });
+    // Catch-all.
   } else {
-    if (
-      error?.message?.includes("rejected") ||
-      error?.message?.includes("denied transaction")
-    ) {
-      onError?.({ code: PayWithCryptoErrorCode.TransactionCancelled, error });
-      postToParent?.({
-        title: PayWithCryptoErrorCode.TransactionCancelled,
-        description: "",
-      });
-    } else if (error?.message?.includes("insufficient funds")) {
-      onError?.({
-        code: PayWithCryptoErrorCode.InsufficientBalance,
-        error,
-      });
-      postToParent?.({
-        title: PayWithCryptoErrorCode.InsufficientBalance,
-        description: `Check your wallet's ETH balance to make sure you have enough`,
-      });
-    } else if (error?.message?.includes("Error switching chain")) {
-      onError?.({
-        code: PayWithCryptoErrorCode.ChainSwitchUnderway,
-        error,
-      });
-      postToParent?.({
-        title: PayWithCryptoErrorCode.ChainSwitchUnderway,
-        description: "Check your wallet app",
-      });
-    } else {
-      onError?.({
-        code: PayWithCryptoErrorCode.ErrorSendingTransaction,
-        error: error.message
-          ? error
-          : new Error(`Something went wrong when sending the transaction`),
-      });
-      postToParent?.({
-        title: PayWithCryptoErrorCode.ErrorSendingTransaction,
-        description:
-          `${error?.message}` ??
-          `Something went wrong when sending the transaction`,
-      });
-    }
+    onError?.({
+      code: PayWithCryptoErrorCode.ErrorSendingTransaction,
+      error: error.message
+        ? error
+        : new Error(`Something went wrong when sending the transaction`),
+    });
+    postToParent?.({
+      title: PayWithCryptoErrorCode.ErrorSendingTransaction,
+      description:
+        `${error?.message}` ??
+        `Something went wrong when sending the transaction`,
+    });
   }
 }
